373d237d40ccd86846f9e0a1b3a7fcd75c238608
Fernando <Fernando@DESKTOP-3EE8PP0>
2022-09-21 22:40:03 +0200

    commit2

---
+++ EjBasic1/src/ejbasic1/EjBasic1.java
@@ -0,0 +1,15 @@
+package ejbasic1;
+
+import java.util.Scanner;
+
+public class EjBasic1 {
+
+	public static void main(String[] args) {
+		Scanner read = new Scanner(System.in);
+		System.out.print("Escribe un número: ");
+		int number = read.nextInt();
+		System.out.print(number);
+
+	}
+
+}
