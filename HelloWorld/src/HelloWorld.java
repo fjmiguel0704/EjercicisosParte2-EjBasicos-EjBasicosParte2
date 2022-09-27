ddc9475460b598b60a5e281d65bd99d473770dc8
Fernando <Fernando@DESKTOP-3EE8PP0>
2022-09-27 20:23:18 +0200

    Primer commit

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//Le mostramos al usuario el siguiente mensaje:
 		System.out.print("Hello World! My name is Fernando José Miguel");
 
 	}
