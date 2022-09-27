acdfc28b4521609129460172348e6db54c08c5ab
Fernando <Fernando@DESKTOP-3EE8PP0>
2022-09-27 20:06:34 +0200

    commit3

--- EjBasic2/src/ejbasic2/EjBasic2.java
+++ EjBasic2/src/ejbasic2/EjBasic2.java
@@ -6,11 +6,19 @@ public class EjBasic2 {
 	
 
 	public static void main(String[] args) {
+		
+		//Creamos el Scanner para que el programa sea capaz de leer todo lo introducido por el usuario
 		Scanner read = new Scanner (System.in);
+		
+		//Vamos a pedirle al usuario que indique su edad
 		System.out.print("¿Cuál es tu edad?");
+		
+		//Declaramos la variable age, que será la edad introducida por el usuario 
 		int age = read.nextInt();
-		System.out.print("El año que viene cumplirás: ");
-		System.out.print(age + 1 + " años");
+		
+		//Mostraremos al usuario la edad que tendrá el año que viene 
+		System.out.print("El año que viene cumplirás: " + ++age + " años");
+		
 
 	}
 
